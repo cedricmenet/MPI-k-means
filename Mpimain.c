d9e0ac3cd3d09195d9cb8f7c17424709cedf84f5
ced.menet@gmail.com <ced.menet@gmail.com>
2016-01-06 11:10:18 +0100

    update

--- Mpimain.c
+++ Mpimain.c
@@ -1,7 +1,9 @@
 /* C Example */
 #include <mpi.h>
 #include <stdio.h>
- 
+
+
+
 int main (int argc, char* argv[])
 {
   int rank, size;
@@ -10,6 +12,7 @@ int main (int argc, char* argv[])
   MPI_Comm_rank (MPI_COMM_WORLD, &rank);        /* get current process id */
   MPI_Comm_size (MPI_COMM_WORLD, &size);        /* get number of processes */
   printf( "Hello world from process %d of %d\n", rank, size );
+  
   MPI_Finalize();
   return 0;
 } 
